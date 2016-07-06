1f8f926157f6dea1bb1832616a0f9b94b8dc50ab
Aaron Walker <a.walker@base2services.com>
2016-07-06 11:33:43 +1000

    switch to use gosu to run as nexus user

--- nexus/docker-entrypoint.sh
+++ nexus/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #ensure we have correct permissions
-sudo chown -R $SONATYPE_WORK
+chown -R nexus:nexus $SONATYPE_WORK
 
-$@
+gosu nexus ${JAVA_HOME}/bin/java \
+  -Dnexus-work=${SONATYPE_WORK} -Dnexus-webapp-context-path=${CONTEXT_PATH} \
+  -Xms${MIN_HEAP} -Xmx${MAX_HEAP} \
+  -cp 'conf/:lib/*' \
+  ${JAVA_OPTS} \
+  org.sonatype.nexus.bootstrap.Launcher ${LAUNCHER_CONF}
