688bbc3c494b59cb30ab6405c169f7a5e32631e5
toshke <n.tosic@base2services.com>
2017-08-25 17:27:36 +1000

    Test docker socket file with -S

--- jenkins-docker-slave/jenkins-docker-slave.sh
+++ jenkins-docker-slave/jenkins-docker-slave.sh
@@ -15,8 +15,11 @@ if [ "$RUN_DOCKER_IN_DOCKER" == "1" ]; then
         --storage-driver=vfs \
         "$@"
 else
-    if [ -f "/var/run/docker.sock" ]; then
-      chown 1000:1000 /var/run/docker.sock
+    if [ -S "/var/run/docker.sock" ]; then
+      docker_gid=$(ls -la /var/run/docker.sock | awk '{print $4}')
+      groupadd -g $docker_gid docker
+      usermod -a -G docker jenkins
+      echo "Added group docker with id $docker_gid and added jenkins user to it"
     fi
     if [ -d "/data/jenkins-dood" ]; then
       chown 1000:1000 /data/jenkins-dood
