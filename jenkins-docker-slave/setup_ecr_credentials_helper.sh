643506f0f2fdb22d35d14500bc9c829a785d3738
toshke <n.tosic@base2services.com>
2017-09-22 14:34:03 +1000

    Docker slave - ecr crednetial helper in dood mode

--- jenkins-docker-slave/setup_ecr_credentials_helper.sh
+++ jenkins-docker-slave/setup_ecr_credentials_helper.sh
@@ -7,9 +7,10 @@ if [ "$USE_ECR_CREDENTIAL_HELPER" == "1" ]; then
   }
 EOF
   mkdir /root/.docker
-  mkdir -p home/jenkins/.docker
+  jenkins_home=$(cat /etc/passwd | grep jenkins | cut -d: -f6)
+  mkdir -p $jenkins_home/.docker
   cp /root/docker.config.ecr.json /root/.docker/config.json
-  cp /root/docker.config.ecr.json /home/jenkins/.docker/config.json
-  chown -R 1000:1000 /home/jenkins/.docker
+  cp /root/docker.config.ecr.json $jenkins_home/.docker/config.json
+  chown -R 1000:1000 $jenkins_home/.docker
   rm /root/docker.config.ecr.json
 fi
