c2f4dc6911acedc3473d2cca458bf1302a6f3da3
Gus <viney-34@hotmail.com>
2018-06-12 17:38:33 +1000

    locust container

---
+++ locust/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+aws s3 cp s3://${SOURCE_BUCKET}/containers/locust/locustfile.py . --region ${REGION} 
+
+chmod +x locustfile.py
+
+LOCUST_MODE=${LOCUST_MODE:-standalone}
+LOCUST_MASTER_BIND_PORT=${LOCUST_MASTER_BIND_PORT:-5557}
+LOCUST_FILE=${LOCUST_FILE:-locustfile.py}
+
+if [ -z ${ATTACKED_HOST+x} ] ; then
+    echo "You need to set the URL of the host to be tested (ATTACKED_HOST)."
+    exit 1
+fi
+
+LOCUST_OPTS="-f ${LOCUST_FILE} --host=${ATTACKED_HOST} --no-reset-stats $LOCUST_OPTS"
+
+case `echo ${LOCUST_MODE} | tr 'a-z' 'A-Z'` in
+"MASTER")
+    LOCUST_OPTS="--master --master-bind-port=${LOCUST_MASTER_BIND_PORT} $LOCUST_OPTS"
+    ;;
+
+"SLAVE")
+    LOCUST_OPTS="--slave --master-host=${LOCUST_MASTER} --master-port=${LOCUST_MASTER_BIND_PORT} $LOCUST_OPTS"
+    if [ -z ${LOCUST_MASTER+x} ] ; then
+        echo "You need to set LOCUST_MASTER."
+        exit 1
+    fi
+    ;;
+esac
+
+cd /locust
+locust ${LOCUST_OPTS}
