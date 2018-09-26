50f9417bef35d9a86c5b28dda60e124f0d3fd314
Arran Ubels <a.ubels@base2services.com>
2018-09-26 11:53:02 +1000

    Unnecessary

--- jenkins/cac/entrypoint.sh
+++ jenkins/cac/entrypoint.sh
@@ -22,12 +22,4 @@ if test -d /inits/; then
     done
 fi
 
-# If init file exists, needs to be overwritten
-# as jenkins exntrypoint does not handle this situation
-# (does not care if init script is updated on)
-if [ -f "/var/jenkins_home/init.groovy.d/custom.groovy" ];then
-  echo "Updating Jenkins init script in /usr/share/jenkins/ref/init.groovy.d/custom.groovy ..."
-  cp /usr/share/jenkins/ref/init.groovy.d/custom.groovy /var/jenkins_home/init.groovy.d/custom.groovy
-fi
-
 exec /usr/local/bin/jenkins.sh
