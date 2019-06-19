86de091ec865ebc8cf0cf276bb8fc5762d3e2857
Guslington <guslington@gmail.com>
2019-06-19 17:00:47 +1000

    easyrsa image for client vpn setup

---
+++ easy-rsa/run.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+if [[ -z $EASYRSA_REQ_CN ]]; then
+  echo "EASYRSA_REQ_CN is not set"
+  exit 1
+fi
+
+if [[ -z $EASYRSA_CLIENT_CN ]]; then
+  echo "EASYRSA_CLIENT_CN is not set"
+  exit 1
+fi
+
+if [[ -z $EASYRSA_OUTPUT_DIR ]]; then
+  EASYRSA_OUTPUT_DIR="output"
+fi
+
+easyrsa init-pki
+easyrsa build-ca nopass
+easyrsa build-server-full server nopass
+easyrsa build-client-full ${EASYRSA_CLIENT_CN} nopass
+
+cp pki/ca.crt $EASYRSA_OUTPUT_DIR/
+cp pki/issued/server.crt $EASYRSA_OUTPUT_DIR/
+cp pki/private/server.key $EASYRSA_OUTPUT_DIR/
+cp pki/issued/${EASYRSA_CLIENT_CN}.crt $EASYRSA_OUTPUT_DIR/
+cp pki/private/${EASYRSA_CLIENT_CN}.key $EASYRSA_OUTPUT_DIR/
